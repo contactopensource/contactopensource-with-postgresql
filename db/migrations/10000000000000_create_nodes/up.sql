0cf7707f41f3de9ee95ea18c3aec8c4c0a544265
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-17 22:48:46 -0700

    Add nodes migration

---
+++ db/migrations/10000000000000_create_nodes/up.sql
@@ -0,0 +1,42 @@
+CREATE TABLE nodes (
+  id uuid not null primary key,
+
+  -- Programming-related
+  tenant_id uuid, -- for optional multi-tenant installation
+  typecast text, -- for optional single table inheritance
+  state text, -- for optional state machine transition
+
+  -- Update-related
+  updated_at_timestamp_utc timestamp, -- example: 2020-01-01T00:00:00 always UTC
+  updated_at_clock_count bigint, -- example: 123456789 as suitable for a vector clock
+  updated_by_text text, -- example: explanation of who updated the contact, why, how, etc.
+
+  -- Meta-related -- the intent is to describe the content fields below.
+  uri text, -- example: 'https://example.com/example.txt'
+
+  -- Content-related -- intended as a mutex of available database types
+  text text, -- example: 'hello world'
+  json jsonb, -- example: '{"hello": "world"}'
+  xml xml, -- example: '<?xml version=\'1.0\'?><example>hello world</example>'
+  number numeric(20,9) -- example: 1234.5678
+
+);
+
+-- Programming-related
+CREATE INDEX ix_items_tenant_id on nodes(tenant_id);
+CREATE INDEX ix_items_typecast on nodes(typecast);
+CREATE INDEX ix_items_state on nodes(state);
+
+-- Update-related
+CREATE INDEX ix_items_updated_at_timestamp_utc on nodes(updated_at_timestamp_utc);
+CREATE INDEX ix_items_updated_at_clock_count on nodes(updated_at_clock_count);
+CREATE INDEX ix_items_updated_by_text on nodes(updated_by_text);
+
+-- Meta-related
+CREATE INDEX ix_items_uri on nodes(uri);
+
+-- Content-related
+CREATE INDEX ix_items_text on nodes(text);
+CREATE INDEX ix_items_json on nodes(json);
+CREATE INDEX ix_items_xml on nodes USING BTREE (cast(xpath('/', xml) as text[]));
+CREATE INDEX ix_items_number on nodes(number);
